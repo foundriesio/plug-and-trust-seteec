844efac6043a3543674f511b3fa19285b89c58a4
Jorge Ramirez-Ortiz <jorge@foundries.io>
2021-12-14 10:09:27 +0100

    se050x: apis: check applet on session open
    Signed-off-by: Jorge Ramirez-Ortiz <jorge@foundries.io>

--- sss/src/se05x/fsl_sss_se05x_apis.c
+++ sss/src/se05x/fsl_sss_se05x_apis.c
@@ -300,30 +300,71 @@ sss_status_t sss_se05x_session_create(sss_se05x_session_t *session,
 #endif
 
 sss_status_t sss_se05x_session_open(sss_se05x_session_t *session,
-    sss_type_t subsystem,
-    uint32_t application_id,
-    sss_connection_type_t connection_type,
-    void *connectionData)
+				    sss_type_t subsystem,
+				    uint32_t application_id,
+				    sss_connection_type_t connection_type,
+				    void *connectionData)
 {
-    sss_status_t retval           = kStatus_SSS_InvalidArgument;
-    SE05x_Connect_Ctx_t *pAuthCtx = NULL;
-    SmCommState_t CommState       = {0};
-    smStatus_t status             = SM_NOT_OK;
-    U16 lReturn;
-    pSe05xSession_t se05xSession;
+	sss_status_t retval           = kStatus_SSS_InvalidArgument;
+	SE05x_Connect_Ctx_t *pAuthCtx = NULL;
+	SmCommState_t CommState       = { 0 };
+	smStatus_t status             = SM_NOT_OK;
+	U16 lReturn;
+	pSe05xSession_t se05xSession;
 #if defined(SMCOM_JRCP_V1_AM)
-    int session_open_retry_cnt = 1;
-    int session_open_retry_dly = 1; //seconds
-    int session_open_retry_cnt_max = 50;
-    int session_open_retry_dly_max = 10; //seconds
+	int session_open_retry_cnt = 1;
+	int session_open_retry_dly = 1; //seconds
+	int session_open_retry_cnt_max = 50;
+	int session_open_retry_dly_max = 10; //seconds
 #endif
 
-    ENSURE_OR_GO_EXIT(session);
-    se05xSession = &session->s_ctx;
+	ENSURE_OR_GO_EXIT(session);
+	se05xSession = &session->s_ctx;
 
-    memset(session, 0, sizeof(*session));
+	memset(session, 0, sizeof(*session));
 
 #if defined(WITH_LIB_SETEEC)
+	uint8_t applet_version[7] = { 0 };
+	size_t applet_versionLen = sizeof(applet_version);
+	U32 appletVersion;
+	smStatus_t ret;
+
+	ret = Se05x_API_GetVersion(NULL, applet_version, &applet_versionLen);
+	if (ret != SM_OK)
+		return kStatus_SSS_Fail;
+
+	if (applet_versionLen == 5 || applet_versionLen == 4 || applet_versionLen == 7) {
+		appletVersion = 0;
+		appletVersion |= applet_version[0];
+		appletVersion <<= 8;
+		appletVersion |= applet_version[1];
+		appletVersion <<= 8;
+		appletVersion |= applet_version[2];
+		appletVersion <<= 8;
+
+		if (HEX_EXPECTED_APPLET_VERSION == (0xFFFFFF00 & appletVersion)) {
+			/* Fine */
+		}
+#if defined(HEX_EXPECTED_APPLET_VERSION_PATCH1)
+		else if (HEX_EXPECTED_APPLET_VERSION_PATCH1 == (0xFFFFFF00 & appletVersion)) {
+			/* Fine */
+		}
+#endif
+		else if ((0xFFFFFF00 & appletVersion) < HEX_EXPECTED_APPLET_VERSION) {
+			LOG_E("Mismatch Applet version.");
+			LOG_E("Compiled for 0x%X. Got older 0x%X",
+			      (HEX_EXPECTED_APPLET_VERSION) >> 8,
+			      (appletVersion) >> 8);
+			LOG_E("Aborting!!!");
+			return kStatus_SSS_Fail;
+		} else {
+			LOG_I("Newer version of Applet Found");
+			LOG_I("Compiled for 0x%X. Got newer 0x%X",
+			      (HEX_EXPECTED_APPLET_VERSION) >> 8,
+			      (appletVersion) >> 8);
+		}
+	}
+
     session->subsystem = subsystem;
     return kStatus_SSS_Success;
 #endif
